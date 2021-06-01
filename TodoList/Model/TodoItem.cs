a10a6da4c1ff685f68c334e9ade080c2846618b7
Cmotoy <Cmotoy@Outlook.com>
2021-06-01 10:14:36 +0100

    adding asp.net core web api todo list

---
+++ TodoList/Model/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Model
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int IsComplete { get; set; }
+        public DateTime date { get; set; }
+    }
+}
---
+++ TodoList/TodoData/TodoAppContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Model;
+
+namespace TodoList.TodoData
+{
+    public class TodoAppContext : DbContext
+    {
+        public TodoAppContext(DbContextOptions<TodoAppContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
